cba7f3789abd991a519cbed3654fa1de65f4b245
od41645 <od41645@ubt-uni.net>
2019-06-26 21:50:07 +0200

    Add dashboard view

---
+++ ClinicWeb/Controllers/StartupController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClinicWeb.Controllers
+{
+    public class StartupController : Controller
+    {
+        // GET: Startup
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
