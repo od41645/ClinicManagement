2e97b13d4fef2622c8186df62e8114b61eb2da46
burbuqebeqiraj <beqirajburbuqe@gmail.com>
2019-06-26 18:51:49 +0200

    Initial stucture of project

---
+++ ClinicWeb/Controllers/DoctorsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClinicDb;
+
+namespace ClinicWeb.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        private DoctorAppointmenEntities db = new DoctorAppointmenEntities();
+
+        // GET: Doctors
+        public ActionResult Index()
+        {
+            var doctors = db.Doctors.Include(d => d.Booking).Include(d => d.Category);
+            return View(doctors.ToList());
+        }
+
+        // GET: Doctors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = db.Doctors.Find(id);
+            if (doctor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctor);
+        }
+
+        // GET: Doctors/Create
+        public ActionResult Create()
+        {
+            ViewBag.Id = new SelectList(db.Bookings, "Appointmentid", "Appointmentid");
+            ViewBag.Id = new SelectList(db.Categories, "Id", "Specialization");
+            return View();
+        }
+
+        // POST: Doctors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Surname,Address,Email,CategoryId")] Doctor doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Doctors.Add(doctor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Id = new SelectList(db.Bookings, "Appointmentid", "Appointmentid", doctor.Id);
+            ViewBag.Id = new SelectList(db.Categories, "Id", "Specialization", doctor.Id);
+            return View(doctor);
+        }
+
+        // GET: Doctors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = db.Doctors.Find(id);
+            if (doctor == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Id = new SelectList(db.Bookings, "Appointmentid", "Appointmentid", doctor.Id);
+            ViewBag.Id = new SelectList(db.Categories, "Id", "Specialization", doctor.Id);
+            return View(doctor);
+        }
+
+        // POST: Doctors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Surname,Address,Email,CategoryId")] Doctor doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(doctor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Id = new SelectList(db.Bookings, "Appointmentid", "Appointmentid", doctor.Id);
+            ViewBag.Id = new SelectList(db.Categories, "Id", "Specialization", doctor.Id);
+            return View(doctor);
+        }
+
+        // GET: Doctors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Doctor doctor = db.Doctors.Find(id);
+            if (doctor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctor);
+        }
+
+        // POST: Doctors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Doctor doctor = db.Doctors.Find(id);
+            db.Doctors.Remove(doctor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
